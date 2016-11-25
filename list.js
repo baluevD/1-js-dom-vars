e61b56b067df1d53821c33f217273a31def14f21
baluevD <newarmlet@gmail.com>
2016-11-25 04:13:52 +0300

    First Task

--- list.js
+++ list.js
@@ -1,2 +1,56 @@
 'use strict';
+var names = ['Вася', 'Дима', 'Катя','Лена','Люся','Люда','Саша','Алеша','Петя','Костя'];
+var jobs = ['Слесарь', 'Менеджер', 'Премьер Министр','Кассир','Инженер',
+    'Сантехник','Риэлтор','Администратор','Продавец','Дворник'];
+var arr = [];
+for(var i = 0; i < 10; i++) {
+    arr.push({
+        'ФИО': names.splice(
+            Math.floor(Math.random() * names.length), 1
+        )[0],
+        'Возраст': Math.floor(Math.random() * 15) + 20,
+        'Профессия': jobs.splice(
+            Math.floor(Math.random() * jobs.length), 1
+        )[0],
+        'Зарплата': Math.floor(Math.random() * 100) + 20
+    })
+}
 
+for(var i = 0; i < arr.length; i++) {
+    var item = document.createElement('li');
+    var block1 = document.createElement('div');
+    var block2 = document.createElement('div');
+    var block3 = document.createElement('div');
+    var block4 = document.createElement('div');
+    block1.innerHTML = arr[i].ФИО;
+    block1.className='ФИО';
+    item.appendChild(block1);
+    block2.innerHTML = arr[i].Возраст;
+    item.appendChild(block2);
+    block3.innerHTML = arr[i].Профессия;
+    item.appendChild(block3);
+    block4.innerHTML = arr[i].Зарплата;
+    item.appendChild(block4);
+    document.body.firstElementChild.appendChild(item);
+    block1.style.display = 'inline-block';
+    block1.style.textDecoration = 'inherit';
+    block2.style.display = 'inline-block';
+    block2.style.textDecoration = 'inherit';
+    block3.style.display = 'inline-block';
+    block3.style.textDecoration = 'inherit';
+    block4.style.display = 'inline-block';
+    block4.style.textDecoration = 'inherit';
+    if((arr[i].Возраст >= 20)&&(arr[i].Возраст < 27)) {
+        block1.style.fontWeight='bold';// {
+    }
+    item.style.backgroundColor = 'green';
+    if((arr[i].ФИО === 'Катя')||(arr[i].ФИО === 'Лена')||(arr[i].ФИО === 'Люда')||(arr[i].ФИО === 'Саша')||(arr[i].ФИО === 'Люся'))
+        item.style.fontSize = '150%';
+    if(arr[i].Зарплата < 50)
+        item.style.backgroundColor = 'red';
+    if((arr[i].Зарплата < 80) && (arr[i].Зарплата >= 50))
+        item.style.backgroundColor = 'yellow';
+    if(arr[i].Профессия === 'Премьер Министр')
+        item.style.textDecoration = 'underline';
+
+}
--- time.js
+++ time.js
@@ -1,2 +1,59 @@
 'use strict';
 
+function  initializeClock(objectDate,arr) {
+    var dateFromArr = '';
+    if (Array.isArray(objectDate)){
+        arr = objectDate;
+    }
+    if(!(objectDate instanceof Date)) {
+        objectDate = new Date();
+    }
+    if(arr===undefined||arr.length==0) {
+        arr = ['hour', 'minute', 'second'];
+    }
+    for(var i=0; i<arr.length;i++) {
+        if (i == 0) {
+            var moreVal = "";
+        }
+        else {
+            var moreVal = ":";
+        }
+        switch (arr[i]) {
+            case 'year':
+                dateFromArr+= moreVal + objectDate.getFullYear();
+                break;
+            case 'month':
+                dateFromArr+= moreVal + objectDate.getMonth();
+                break;
+            case 'day':
+                dateFromArr+= moreVal + objectDate.getDay();
+                break;
+            case 'hour':
+                dateFromArr+= moreVal + objectDate.getHours();
+                break;
+            case 'minute':
+                dateFromArr+= moreVal + objectDate.getMinutes();
+                break;
+            case 'second':
+                dateFromArr+= moreVal + objectDate.getSeconds();
+                break;
+            case 'millisecond':
+                dateFromArr+= moreVal + objectDate.getMilliseconds();
+                break;
+        }
+    }
+    document.getElementById('time').innerHTML = dateFromArr;
+}
+// initializeClock(new Date(2014, 6, 14));
+// initializeClock(['hour', 'second']);
+// initializeClock(new Date(2014, 6, 14), ['year', 'month', 'second']);
+// initializeClock();
+
+var a = [];
+
+var goStart = function(){
+    var today = new Date();
+    initializeClock(today, a);
+}
+
+setInterval(function(){goStart()}, 1000)
