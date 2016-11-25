d316d09568a0395dfc0a83902b187c0fe84e935a
baluevD <newarmlet@gmail.com>
2016-11-25 19:10:16 +0300

    Good code

--- list.js
+++ list.js
@@ -1,56 +1,56 @@
 'use strict';
-var names = ['Вася', 'Дима', 'Катя','Лена','Люся','Люда','Саша','Алеша','Петя','Костя'];
-var jobs = ['Слесарь', 'Менеджер', 'Премьер Министр','Кассир','Инженер',
-    'Сантехник','Риэлтор','Администратор','Продавец','Дворник'];
-var arr = [];
-for(var i = 0; i < 10; i++) {
-    arr.push({
-        'ФИО': names.splice(
+var names = ['Вася', 'Дима', 'Катя', 'Лена', 'Люся', 'Люда', 'Саша', 'Алеша', 'Петя', 'Костя'];
+var namesFemale = ['Катя', 'Лена', 'Люся', 'Люда', 'Саша'];
+var jobs = ['Слесарь', 'Менеджер', 'Премьер Министр', 'Кассир', 'Инженер',
+    'Сантехник', 'Риэлтор', 'Администратор', 'Продавец', 'Дворник'
+];
+var arrPeople = [];
+for (var i = 0; i < 10; i++) {
+    arrPeople.push({
+        name: names.splice(
             Math.floor(Math.random() * names.length), 1
         )[0],
-        'Возраст': Math.floor(Math.random() * 15) + 20,
-        'Профессия': jobs.splice(
+        age: Math.floor(Math.random() * 15) + 20,
+        profession: jobs.splice(
             Math.floor(Math.random() * jobs.length), 1
         )[0],
-        'Зарплата': Math.floor(Math.random() * 100) + 20
+        salary: Math.floor(Math.random() * 100) + 20
     })
 }
 
-for(var i = 0; i < arr.length; i++) {
+for (var i = 0; i < arrPeople.length; i++) {
     var item = document.createElement('li');
-    var block1 = document.createElement('div');
-    var block2 = document.createElement('div');
-    var block3 = document.createElement('div');
-    var block4 = document.createElement('div');
-    block1.innerHTML = arr[i].ФИО;
-    block1.className='ФИО';
-    item.appendChild(block1);
-    block2.innerHTML = arr[i].Возраст;
-    item.appendChild(block2);
-    block3.innerHTML = arr[i].Профессия;
-    item.appendChild(block3);
-    block4.innerHTML = arr[i].Зарплата;
-    item.appendChild(block4);
+    var blockName = document.createElement('div');
+    var blockAge = document.createElement('div');
+    var blockProfession = document.createElement('div');
+    var blockSalary = document.createElement('div');
+    blockName.innerHTML = arrPeople[i].name;
+    blockName.className = 'name';
+    item.appendChild(blockName);
+    blockAge.innerHTML = arrPeople[i].age;
+    item.appendChild(blockAge);
+    blockProfession.innerHTML = arrPeople[i].profession;
+    item.appendChild(blockProfession);
+    blockSalary.innerHTML = arrPeople[i].salary;
+    item.appendChild(blockSalary);
     document.body.firstElementChild.appendChild(item);
-    block1.style.display = 'inline-block';
-    block1.style.textDecoration = 'inherit';
-    block2.style.display = 'inline-block';
-    block2.style.textDecoration = 'inherit';
-    block3.style.display = 'inline-block';
-    block3.style.textDecoration = 'inherit';
-    block4.style.display = 'inline-block';
-    block4.style.textDecoration = 'inherit';
-    if((arr[i].Возраст >= 20)&&(arr[i].Возраст < 27)) {
-        block1.style.fontWeight='bold';// {
-    }
+    blockName.style.display = 'inline-block';
+    blockName.style.textDecoration = 'inherit';
+    blockAge.style.display = 'inline-block';
+    blockAge.style.textDecoration = 'inherit';
+    blockProfession.style.display = 'inline-block';
+    blockProfession.style.textDecoration = 'inherit';
+    blockSalary.style.display = 'inline-block';
+    blockSalary.style.textDecoration = 'inherit';
+    if ((arrPeople[i].age >= 20) && (arrPeople[i].age < 27))
+        blockName.style.fontWeight = 'bold';
     item.style.backgroundColor = 'green';
-    if((arr[i].ФИО === 'Катя')||(arr[i].ФИО === 'Лена')||(arr[i].ФИО === 'Люда')||(arr[i].ФИО === 'Саша')||(arr[i].ФИО === 'Люся'))
-        item.style.fontSize = '150%';
-    if(arr[i].Зарплата < 50)
+    if(namesFemale.indexOf(arrPeople[i].name) >= 0)
+       item.style.fontSize = '150%';
+    if (arrPeople[i].salary < 50)
         item.style.backgroundColor = 'red';
-    if((arr[i].Зарплата < 80) && (arr[i].Зарплата >= 50))
+    if ((arrPeople[i].salary < 80) && (arrPeople[i].salary >= 50))
         item.style.backgroundColor = 'yellow';
-    if(arr[i].Профессия === 'Премьер Министр')
+    if (arrPeople[i].profession === 'Премьер Министр')
         item.style.textDecoration = 'underline';
-
 }
